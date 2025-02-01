476a071d9431eebd021cfac4f6f843140e1b3c3f
programacaorj <moises.rjandrade@gmail.com>
2025-02-01 13:55:27 +0000

    Adiconei a opção "informe o estado"

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -10,16 +10,20 @@ int main() {
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
+    char estado[20];
     char codigo[6]; 
     char nome[50];
     int populacao;
-    double area, pib; 
+    float area, pib; 
     int pontos_turisticos; 
 
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
     
+    printf("informe o estado: \n");
+    scanf("%s", estado);
+   
     printf("Digite o codigo da cidade: \n");
     scanf("%s", codigo);
 
@@ -31,11 +35,11 @@ int main() {
     getchar(); //limpa o buffer
 
     printf("Digite a area da cidade: \n"); 
-    scanf(" %lf", &area);
+    scanf(" %f", &area);
     getchar();//limpa o buffer
 
     printf("Digite o pib da cidade: \n");
-    scanf(" %lf", &pib);
+    scanf(" %f", &pib);
     getchar(); //limpa o buffer 
 
     printf("Quantidade de pontos turisticos: \n"); 
@@ -45,13 +49,14 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
+    printf("Estado: %s \n", estado);
     printf("Código da carta: %s \n", codigo);
     printf("Nome da cidade: %s \n", nome);
     printf("População: %d \n", populacao);
-    printf("Área: %1f Km² \n", area);
-    printf("PIB: %1f \n", pib);
+    printf("Área: %f Km² \n", area);
+    printf("PIB: %f bi \n", pib);
     printf("Pontos turísticos: %d \n", pontos_turisticos);
-    printf("CARTA CADASTRADA!");
+    printf("CARTA CADASTRADA! \n");
 
     return 0;
 }
